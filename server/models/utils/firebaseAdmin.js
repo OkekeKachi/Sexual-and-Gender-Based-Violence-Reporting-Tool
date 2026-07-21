d32198d51664fc981067cf16db94d23986628926
OkekeKachi <141140531+OkekeKachi@users.noreply.github.com>
2026-07-21 15:52:12 +0100

    updated firebaseAdmin

--- server/models/utils/firebaseAdmin.js
+++ server/models/utils/firebaseAdmin.js
@@ -1,9 +1,13 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./safespeak-21f59-firebase-adminsdk-829uf-265fd69059.json"); // download from Firebase
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 module.exports = admin;
